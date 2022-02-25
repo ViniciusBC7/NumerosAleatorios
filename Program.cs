d38d7704f8bac16c37642c37f45487495194fb8e
Vinicius Costa <oviniciusc@gmail.com>
2022-02-24 22:36:50 -0300

    Update
    Código pronto e funcional.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random aleatorio = new Random();
+        int valor = aleatorio.Next(1, 10);
+        Console.WriteLine($"O Número sorteado é: {valor}");
+    }
+}
